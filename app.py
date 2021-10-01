84bb182e234683eb80fd383aec69d68dca77d9c8
merlin-201 <npradeeppatil2001@gmail.com>
2021-10-02 03:54:15 +0530

    bot-setup

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'cat' in incoming_msg:
+        msg.body("Meow Meow")
+        responded = True
+    if 'dog' in incoming_msg:
+        msg.body("Woof Woof")
+        responded = True
+    if not responded:
+        msg.body('i hate humans')
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run()
