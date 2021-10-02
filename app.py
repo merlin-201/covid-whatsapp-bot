0528eab487cdebe5408608f067437cc8749bf138
merlin-201 <npradeeppatil2001@gmail.com>
2021-10-02 11:50:24 +0530

    done

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import jsonify
 import json
+from database import upload_json
 
 app = Flask(__name__)
+
 
 @app.route('/',methods=["GET"])
 def hello():
     return "hello there"
 
-initiated = False
 
 def write_suffix(msg):
     with open('suffix.txt', 'w') as f:
@@ -46,8 +47,7 @@ def bot():
 
     if incoming_msg == "/bed":
         msg.body('''
-        Hello, Welcome to the Covid bed allocation helpline.
-        Please provide me your basic details :
+        Hello, Welcome to the Covid bed allocation helpline.\nPlease provide me your name :
         ''')
         write_suffix('/name')
 
@@ -63,7 +63,12 @@ def bot():
         contact = incoming_msg[9:]
         add_field('contact',contact)
         msg.body('''
-        Contact Added\n\n
+        Contact Added\n Type "confirm" if these are your details
+        ''')
+    if incoming_msg == "confirm":
+        upload_json()
+        msg.body('''
+        your details have been succesfully uploaded\n\nOur Call Team will reach out to you soon
         ''')
 
     return str(resp)
---
+++ database.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+import json
+
+conn_str = "mongodb+srv://rahul:rahul@cluster0.rb9pz.mongodb.net/covid?retryWrites=true&w=majority"
+
+client = MongoClient(conn_str)
+# print(client.list_database_names())
+
+db = client['covid']
+# print(db.list_collection_names())
+
+bedrequests = db['bedrequests']
+
+def upload_json():
+    with open('data.json','r') as f:
+        data = json.load(f)
+
+    bedrequests.insert_one(data)
+
+# clear_data()
+upload_json()
